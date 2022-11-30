d7e8c861124c117146790b627281029f5869220f
ItsAlanK <alankinsella1993@gmail.com>
2022-11-30 21:54:48 +0000

    Install react bootstrap & fontawesome icons and create NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,28 @@
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons"
+
+export const NavBar = () => {
+    return (
+        <Navbar bg="light" variant="light">
+          <Container>
+            <Navbar.Brand href="#home">ItsAlanK</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav">
+                <span className='navbar-toggler-icon'></span>
+            </Navbar.Toggle>
+            <Nav className="me-auto">
+              <Nav.Link href="#projects">Projects</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+              <Nav.Link href="#about">About</Nav.Link>
+            </Nav>
+            <span className='navbar-text'>
+                <div className='social-icon'>
+                    <a href='#'><FontAwesomeIcon icon={ faGithub }/></a>
+                    <a href='#'><FontAwesomeIcon icon={ faInstagram }/></a>
+                    <a href='#'><FontAwesomeIcon icon={ faLinkedin }/></a>
+                </div>
+            </span>
+          </Container>
+        </Navbar>
+    )
+}
