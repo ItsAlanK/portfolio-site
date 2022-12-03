aa136974f37f02339f927c1774c9c8c27a7b11bb
ItsAlanK <alankinsella1993@gmail.com>
2022-12-03 14:16:51 +0000

    Add hover effects to navbar links and linked socials

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,16 +10,16 @@ export const NavBar = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav">
                 <span className='navbar-toggler-icon'></span>
             </Navbar.Toggle>
-            <Nav className="me-auto">
+            <Nav className='ms-auto'>
               <Nav.Link href="#projects">Projects</Nav.Link>
               <Nav.Link href="#contact">Contact</Nav.Link>
               <Nav.Link href="#about">About</Nav.Link>
             </Nav>
             <span className='navbar-text'>
                 <div className='social-icon'>
-                    <a class='nav-social' href='#'><FontAwesomeIcon icon={ faGithub }/></a>
-                    <a class='nav-social' href='#'><FontAwesomeIcon icon={ faInstagram }/></a>
-                    <a class='nav-social' href='#'><FontAwesomeIcon icon={ faLinkedin }/></a>
+                    <a class='nav-social' href='https://github.com/ItsAlanK' target='_blank'><FontAwesomeIcon icon={ faGithub }/></a>
+                    <a class='nav-social' href='https://www.instagram.com/alankinsella_/' target='_blank'><FontAwesomeIcon icon={ faInstagram }/></a>
+                    <a class='nav-social' href='https://www.linkedin.com/in/alankinsella25/' target='_blank'><FontAwesomeIcon icon={ faLinkedin }/></a>
                 </div>
             </span>
           </Container>
