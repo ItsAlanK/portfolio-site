d82935b11484ad5d1333f95db509b9a06cfcba7c
ItsAlanK <alankinsella1993@gmail.com>
2022-12-03 16:11:50 +0000

    Add animation to toggler. Bootstrap not changing 'aria-expanded' so created own function to toggle true/false and trigger animation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,29 +6,42 @@ import { faGithub, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-
 export const NavBar = () => {
   const [activeLink, setActiveLink] = useState('home');
 
-  const onUpdateActiveLink = (value) => {
-    setActiveLink(value);
+  const onUpdateActiveLink = async (value) => {
+    await setActiveLink(value);
+  }
+
+  const [selectAria, setSelectAria] = useState(false);
+
+  const handleChangeSelect = () => {
+    if (selectAria == true) {
+      setSelectAria(false);
+    }
+    else {
+      setSelectAria(true);
+    }
   }
 
   return (
-      <Navbar>
+      <Navbar expand='md'>
         <Container>
-          <Navbar.Brand href="#home" className={ activeLink === 'home' ? 'active' : '' } onClick={() => onUpdateActiveLink('home')}>ITSALANK</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <Navbar.Brand href="#home">ITSALANK</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" aria-expanded={ selectAria } onClick={() => handleChangeSelect()}>
               <span className='navbar-toggler-icon'></span>
           </Navbar.Toggle>
-          <Nav className='ms-auto'>
-            <Nav.Link href="#about" className={ activeLink === 'about' ? 'active navbar-link' : 'navbar-link' } onClick={() => onUpdateActiveLink('about')}>About</Nav.Link>
-            <Nav.Link href="#projects" className={ activeLink === 'projects' ? 'active navbar-link' : 'navbar-link' } onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
-            <Nav.Link href="#contact" className={ activeLink === 'contact' ? 'active navbar-link' : 'navbar-link' } onClick={() => onUpdateActiveLink('contact')}>Contact</Nav.Link>
-          </Nav>
-          <span className='navbar-text'>
-              <div className='social-icon'>
-                  <a class='nav-social' href='https://github.com/ItsAlanK' target='_blank'><FontAwesomeIcon icon={ faGithub }/></a>
-                  <a class='nav-social' href='https://www.instagram.com/alankinsella_/' target='_blank'><FontAwesomeIcon icon={ faInstagram }/></a>
-                  <a class='nav-social' href='https://www.linkedin.com/in/alankinsella25/' target='_blank'><FontAwesomeIcon icon={ faLinkedin }/></a>
-              </div>
-          </span>
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <Nav.Link href="#about" className={ activeLink === 'about' ? 'active' : '' } onClick={() => onUpdateActiveLink('about')}>About</Nav.Link>
+              <Nav.Link href="#projects" className={ activeLink === 'projects' ? 'active' : '' } onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+              <Nav.Link href="#contact" className={ activeLink === 'contact' ? 'active' : '' } onClick={() => onUpdateActiveLink('contact')}>Contact</Nav.Link>
+            </Nav>
+            <span className='navbar-text'>
+                <div className='social-icon'>
+                    <a class='nav-social' href='https://github.com/ItsAlanK' target='_blank'><FontAwesomeIcon icon={ faGithub }/></a>
+                    <a class='nav-social' href='https://www.instagram.com/alankinsella_/' target='_blank'><FontAwesomeIcon icon={ faInstagram }/></a>
+                    <a class='nav-social' href='https://www.linkedin.com/in/alankinsella25/' target='_blank'><FontAwesomeIcon icon={ faLinkedin }/></a>
+                </div>
+            </span>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
   )
