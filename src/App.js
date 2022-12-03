cf575a67ed5340e8f3b178efa06ba00c5514b34b
ItsAlanK <alankinsella1993@gmail.com>
2022-12-03 18:38:55 +0000

    Add shadow effect to logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './style/styles.js';
 import { NavBar } from './components/NavBar.js';
+import { Hero } from './components/Hero.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,14 @@
+import { Container, Row, Col  } from "react-bootstrap"
+
+export const Hero = () => {
+    return (
+        <section class='hero' id='home'>
+            <Container>
+                <Row>
+                    <Col>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
