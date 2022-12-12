d9d7bc7134274d91954f75e3453786e5cb216236
ItsAlanK <alankinsella1993@gmail.com>
2022-12-12 08:54:02 +0000

    Add cols and rows structure to contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 const FORM_ENDPOINT = "https://public.herotofu.com/v1/cc82b800-798a-11ed-a126-b172cf164538"; // TODO - fill on the later step
 
@@ -20,27 +21,34 @@ export const Contact = () => {
   }
 
   return (
-    <div>
-      <iframe title='dummyframe' name="dummyframe" id="dummyframe"></iframe>
-      <form
-        action={FORM_ENDPOINT}
-        onSubmit={handleSubmit}
-        method="POST"
-        target='dummyframe'
-      >
-        <div>
-          <input type="text" placeholder="Your name" name="name" required />
-        </div>
-        <div>
-          <input type="email" placeholder="Email" name="email" required />
-        </div>
-        <div>
-          <textarea placeholder="Your message" name="message" required />
-        </div>
-        <div>
-          <button type="submit"> Send a message </button>
-        </div>
-      </form>
-    </div>
+    <section className="contact" id="contact">
+      <Container>
+        <Row className="align-items-center flex">
+          <Col size={12} md={6}>
+            <iframe title='dummyframe' name="dummyframe" id="dummyframe"></iframe>
+            <h1 className="title">Contact Me</h1>
+            <form
+              action={FORM_ENDPOINT}
+              onSubmit={handleSubmit}
+              method="POST"
+              target='dummyframe'
+            >
+              <Row className='form-row'>
+                <Col size={12} className="px-1">
+                  <input type="text" placeholder="Your name" name="name" required />
+                </Col>
+                <Col size={12} className="px-1">
+                  <input type="email" placeholder="Email" name="email" required />
+                </Col>
+                <Col size={12} className="px-1">
+                  <textarea placeholder="Your message" name="message" required />
+                  <button type="submit"> Send a message </button>
+                </Col>
+              </Row>
+            </form>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   );
 };
