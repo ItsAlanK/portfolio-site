42f62d198bafd46ec74512ef7a1e770e7b636cd9
ItsAlanK <alankinsella1993@gmail.com>
2023-01-15 15:56:43 +0000

    Add starter content to about section paragraph

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export const About = () => {
                     <Col sm={12} md={10} lg={8} className="text-center about-col">
                         <h1 className="title">About</h1>
                         <p>
-                            Hey, my name is Alan. Thanks for stopping by. This is where I'm going to put a little bit of an intro about myself and what I do.
+                            Hey, my name's Alan. I'm currently a Support Advisor for Shopify as well as a software dev and generally creative person. I have a Bsc Hons in Multimedia from Dublin City University which gave me experience and knowledge in all areas of media creation. I've also studied Digital Marketing and Data Analytics. And most recently I've finished my Professional Diploma in Full Stack Software Development from Code Institute.
                         </p>
                     </Col>
                 </Row>
